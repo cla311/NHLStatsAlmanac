c7ea1fcde8e7b52b6f71a6c922d5419d6a60dbef
Ryan Donnelly <rdonnelly98@gmail.com>
2018-11-24 13:49:47 -0800

    testing insert

--- almanac/userteam.php
+++ almanac/userteam.php
@@ -41,6 +41,7 @@ $teamID = trim($_GET['fantasyTeamID']);
 if (!empty($_SESSION['playerID']) && !empty($_SESSION['name']))
 {
     $sql_insert_player = "INSERT INTO $fantasyTeamID VALUES ($fantasyTeamID,$username,$playerID,$title)";
+    echo $sql_insert_player;
     $res = $db->query($sql_insert_player);
     
     // unset session values
