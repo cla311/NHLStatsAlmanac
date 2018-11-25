05e90fab391605c5fed16981a6e7ca6bcea6d033
Ryan Donnelly <rdonnelly98@gmail.com>
2018-11-24 17:03:23 -0800

    merge

--- almanac/userteam.php
+++ almanac/userteam.php
@@ -1,5 +1,4 @@
-<?php
-
+<?php 
 require('../required/nav.php');
 require('../required/functions.php');
 session_start();
@@ -33,45 +32,34 @@ if (!empty($_SESSION['playerID']) && !empty($_SESSION['name'])) {
 }
 ?>
 
-<?php require_login(); // if not logged in, redirect to login page   ?>
+<?php require_login(); // if not logged in, redirect to login page ?>
 
 <?php
-
 $teamID = trim($_GET['fantasyTeamID']);
 
 // insert player into fantasy team table
-<<<<<<< HEAD
 if (!empty($_SESSION['playerID']) && !empty($_SESSION['name']))
 {
     $sql_insert_player = "INSERT INTO $fantasyTeamID VALUES ('".$fantasyTeamID."','".$username."','".$playerID."','".$title."')";
-=======
-if (!empty($_SESSION['playerID']) && !empty($_SESSION['name'])) {
-    $sql_insert_player = "INSERT INTO $fantasyTeamID VALUES ($fantasyTeamID,$username,$playerID,$title)";
->>>>>>> 4351fbdd835e449270218d35b00b855e77df664c
     echo $sql_insert_player;
     $res = $db->query($sql_insert_player);
-
+    
     // unset session values
     unset($_SESSION['playerID']);
     unset($_SESSION['name']);
 }
 
-<<<<<<< HEAD
 $query = "SELECT $fantasyTeamID.playerID, player.name, player.position FROM $fantasyTeamID INNER JOIN player ON $fantasyTeamID.playerID = player.playerID WHERE $fantasyTeamID.playerID = player.playerID";
 echo $query;
 $res = $db->query($query);
 
 echo "<h3>".$title."</h3>";
-=======
-echo "<h3>" . $title . "</h3>";
->>>>>>> 4351fbdd835e449270218d35b00b855e77df664c
 
 echo "<table border=\"solid\">";
 echo "<tr>";
 echo "<th>Players</th>";
 echo "<th>Position</th>";
 echo "</tr>";
-<<<<<<< HEAD
 while ($row = $res->fetch_row())
 {
     echo "<tr>";
@@ -83,9 +71,7 @@ while ($row = $res->fetch_row())
     echo "</td>";
     echo "</tr>";
 }
-=======
->>>>>>> 4351fbdd835e449270218d35b00b855e77df664c
 echo "</table>";
 
-echo "<a href=\"lookup.php\">Search Players</a>";
+echo "<a href=\"lookup.php\">Search Players</a>"; 
 ?>
