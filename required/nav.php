9fd1ec5a133d7312558fb7b4aa967d00f3ba6321
Ryan Donnelly <rdonnelly98@gmail.com>
2018-11-28 12:39:30 -0800

    nav header stuff

--- required/nav.php
+++ required/nav.php
@@ -1,19 +1,23 @@
 <head>
 <link rel="stylesheet" href="../css/main.css">
+<link rel="stylesheet" href="../css/grid.css">
 </head>
 
 <?php
 $host = $_SERVER['HTTP_HOST']
 ?>
 
+<img class="logo" src="../img/hockey.png" alt="Website Logo" />
+
 <header>
     <nav>
         <!-- set each link to http protocol, NOT https -->
         <?php
-        echo "<a href=\"http://" . $host . "/NHLStatsAlmanac/almanac/index.php\">Home</a> ";
-        echo "<a href=\"http://" . $host . "/NHLStatsAlmanac/almanac/lookup.php\">Search</a> ";
-        echo "<a href=\"http://" . $host . "/NHLStatsAlmanac/almanac/fantasy.php\">Fantasy Teams</a> ";
-        echo "<a href=\"http://" . $host . "/NHLStatsAlmanac/almanac/login.php\">Login/Logout</a>";
+        echo "<a class=\"nav-item\" href=\"http://" . $host . "/NHLStatsAlmanac/almanac/index.php\">Home</a> ";
+        echo "<a class=\"nav-item\" href=\"http://" . $host . "/NHLStatsAlmanac/almanac/lookup.php\">Search</a> ";
+        echo "<a class=\"nav-item\" href=\"http://" . $host . "/NHLStatsAlmanac/almanac/fantasy.php\">Fantasy Teams</a> ";
+        echo "<a class=\"nav-item\" href=\"http://" . $host . "/NHLStatsAlmanac/almanac/login.php\">Login/Logout</a>";
+        echo "<a class=\"nav-item\" href=\"http://" . $host . "/NHLStatsAlmanac/almanac/account.php\">My Account</a>";
         ?>
     </nav>
 
