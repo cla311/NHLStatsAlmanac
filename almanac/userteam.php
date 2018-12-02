a6b0e4ec3817d01d0c34c0679d6081d880a79741
Ryan Donnelly <rdonnelly98@gmail.com>
2018-12-02 13:28:28 -0800

    don't need to be logged in when viewing fantasy teams

--- almanac/userteam.php
+++ almanac/userteam.php
@@ -33,16 +33,7 @@ if (!empty($_SESSION['playerID']) && !empty($_SESSION['name'])) {
     $playerName = $_SESSION['name'] = [];
 }
 ?>
-
-<<<<<<< HEAD
-<<<<<<< HEAD
 <?php // require_login(); // if not logged in, redirect to login page  ?>
-=======
-<?php require_login(); // if not logged in, redirect to login page   ?>
->>>>>>> 5894542464eb510e7fb2b824a0caac155b18dc96
-=======
-<?php require_login(); // if not logged in, redirect to login page   ?>
->>>>>>> 5894542464eb510e7fb2b824a0caac155b18dc96
 
 <?php
 
